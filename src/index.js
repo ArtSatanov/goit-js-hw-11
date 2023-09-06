a5fbe65bdeb4749d2583e3489f9156d54d341158
ArtSatanov <artsatanov@gmail.com>
2023-09-06 20:44:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,8 +19,9 @@ async function onSubmit(event) {
 
    try {
       const request = await serverRequest(searchQueries);
-      console.log(request);
-      // refs.container.innerHTML = createMarkup(request);
+      console.log(request.data.hits);
+      refs.container.innerHTML = createMarkup(request.data.hits);
+      console.log(createMarkup(request.data.hits));
    }
 
    catch {
@@ -34,15 +35,42 @@ async function serverRequest(searchQueries) {
       const  response = await axios.get('https://pixabay.com/api/', {
          params: {
          key: '39273189-3f8e43104201480407e7841b8',
-         q: searchQueries
+            q: searchQueries,
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: 'safesearch',
          }
       })
    try {
-      console.log(response);
       return response;
 
    } catch (err) {
       Notify.failure('Sorry, there are no images matching your search query. Please try again.');
    }
    
+}
+
+function createMarkup(arr) {
+   return arr.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
+      `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      ${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+      ${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+      ${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+      ${downloads}
+    </p>
+  </div>
+</div>`).join("");
 }
